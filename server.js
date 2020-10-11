4f6b7685bb52ecc2a9ad13a92bd90f2ffaa25f5f
ɃЯV丶Professorᴰᴱᶻ ⏦ <64661206+IndianProfessor@users.noreply.github.com>
2020-10-11 23:01:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-const Discord = require('discord.js');
+const Discord = require('v11-discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
 console.log(`Logged in as ${client.user.tag}!`);
 console.log("DISCORD BOT DEVLOPER")
 
-client.user.setActivity(`ok`, {
-type: "STREAMING",
-url: "https://www.youtube.com/channel/UCF8EBnJskNhd-JON7D4CgpA"})
-    .then(presence => console.log(`DM BOT OF HYPE OFFICIAL  ${presence.game ? presence.game.none : 'DISCORD BOT DEVLOPER'}`))
+client.user.setActivity(`with ʜყ℘г Fam`, {
+status: "idle",
+type: "Playing",
+url: "https://www.twitch.tv/discord.gg"})
+client.user.setStatus('idle')
+    .then(presence => console.log(`HYPR OFFICIAL ${presence.game ? presence.game.none : 'DISCORD BOT DEVLOPER'}`))
     .catch(console.error);
-  let channel = client.channels.get('709459156125614080'); 
-channel.join()
 });
 
-client.login('NjkxOTAzNjM4NTk1MTc0NDMw.X1sgrg.cArZ9JLpQNbRU_7tOb4Iw6ghLTs');
- 
+client.login('NzY0ODc1NTI0OTEyMTg1Mzg0.X4MpNA.DocLYG_rvaPPPCyO8j0_pTu24Dk');
